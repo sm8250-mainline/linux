8fb1c1cce98d2947c6466be993e6f220973457a8
BotchedRPR <thenxguy0@gmail.com>
2022-10-08 13:40:32 +0200

    drivers: input-touchscreen: replace file_operations with proc_ops

--- drivers/input/touchscreen/ft3658_base.c
+++ drivers/input/touchscreen/ft3658_base.c
@@ -279,10 +279,9 @@ proc_read_err:
 	return ret;
 }
 
-static const struct file_operations fts_proc_fops = {
-	.owner  = THIS_MODULE,
-	.read   = fts_debug_read,
-	.write  = fts_debug_write,
+static const struct proc_ops fts_proc_fops = {
+	.proc_read   = fts_debug_read,
+	.proc_write  = fts_debug_write,
 };
 #else
 static int fts_debug_write(
@@ -517,8 +516,8 @@ static ssize_t fts_fw_version_read(struct file *filp,
 		return cnt;
 }
 
-static const struct file_operations tp_fw_version_fops = {
-	.read = fts_fw_version_read,
+static const struct proc_ops tp_fw_version_fops = {
+	.proc_read = fts_fw_version_read,
 };
 
 
@@ -546,8 +545,8 @@ static ssize_t fts_lockdown_info_read(struct file *filp,
 		return cnt;
 }
 
-static const struct file_operations tp_lockdown_info_fops = {
-	.read = fts_lockdown_info_read,
+static const struct proc_ops tp_lockdown_info_fops = {
+	.proc_read = fts_lockdown_info_read,
 };
 
 
