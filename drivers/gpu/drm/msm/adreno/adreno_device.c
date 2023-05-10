ff2ba2f2d1a03b145ba13ea7a99093a2dd479a83
Konrad Dybcio <konrad.dybcio@linaro.org>
2023-05-10 11:57:26 +0200

    drm/msm/a6xx: Add A730 support
    Add support for Adreno 730, also known as GEN7_0_x, found on SM8450.
    
    Signed-off-by: Konrad Dybcio <konrad.dybcio@linaro.org>

--- drivers/gpu/drm/msm/adreno/adreno_device.c
+++ drivers/gpu/drm/msm/adreno/adreno_device.c
@@ -381,6 +381,19 @@ static const struct adreno_info gpulist[] = {
 		.zapfw = "a690_zap.mdt",
 		.hwcg = a690_hwcg,
 		.address_space_size = SZ_16G,
+	}, {
+		.rev = ADRENO_REV(7, 3, 0, ANY_ID),
+		.revn = 730,
+		.fw = {
+			[ADRENO_FW_SQE] = "a730_sqe.fw",
+			[ADRENO_FW_GMU] = "gmu_gen70000.bin",
+		},
+		.gmem = SZ_2M,
+		.inactive_period = DRM_MSM_INACTIVE_PERIOD,
+		.init = a6xx_gpu_init,
+		.zapfw = "a730_zap.mdt",
+		.hwcg = a730_hwcg,
+		.address_space_size = SZ_16G,
 	},
 };
 
