abcf1825ccebc20e46526d281587b08525af7961
David Wronek <davidwronek@gmail.com>
2023-07-13 12:56:56 +0200

    Ignore dual DSI for now
    Can I get half of the panel working?

--- drivers/gpu/drm/panel/panel-raydium-rm69380.c
+++ drivers/gpu/drm/panel/panel-raydium-rm69380.c
@@ -34,7 +34,7 @@ struct panel_info {
 	struct mipi_dsi_device *dsi[2];
 	const struct panel_desc *desc;
 	struct gpio_desc *reset_gpio;
-	struct regulator *vddio;
+	struct regulator_bulk_data supplies[2];
 
 	bool prepared;
 };
@@ -63,7 +63,6 @@ static inline struct panel_info *to_panel_info(struct drm_panel *panel)
 	return container_of(panel, struct panel_info, panel);
 }
 
-//90Hz?
 static int j716f_edo_init_sequence(struct panel_info *pinfo)
 {
 	struct mipi_dsi_device *dsi0 = pinfo->dsi[0];
@@ -91,39 +90,6 @@ static int j716f_edo_init_sequence(struct panel_info *pinfo)
 
 	return 0;
 }
-
-//60Hz?
-/*static int j716f_edo_init_sequence(struct panel_info *pinfo)
-{
-	struct mipi_dsi_device *dsi0 = pinfo->dsi[0];
-	struct mipi_dsi_device *dsi1 = pinfo->dsi[1];
-	int i;
-
-	for (i = 0; i < DSI_NUM_MIN + pinfo->desc->is_dual_dsi; i++) {
-		pinfo->dsi[i]->mode_flags |= MIPI_DSI_MODE_LPM;
-	}
-
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xfe, 0xd4);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x00, 0x80);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xfe, 0xd0);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x48, 0x00);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xfe, 0x26);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x75, 0x3f);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x1d, 0x1a);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xfe, 0x40);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xbd, 0x05);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xfe, 0x00);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x53, 0x28);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xc2, 0x08);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x35, 0x00);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x51, 0x07, 0xff);
-	msleep(20);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x11, 0x00);
-	msleep(36);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x29, 0x00);
-
-	return 0;
-}*/
 
 static const struct drm_display_mode j716f_edo_modes[] = {
 	{
@@ -155,6 +121,7 @@ static const struct panel_desc j716f_edo_desc = {
 	.mode_flags = MIPI_DSI_MODE_VIDEO_BURST | MIPI_DSI_CLOCK_NON_CONTINUOUS | MIPI_DSI_MODE_LPM,
 	.init_sequence = j716f_edo_init_sequence,
 	.is_dual_dsi = true,
+	.has_dcs_backlight = true,
 };
 
 static void rm69380_reset(struct panel_info *pinfo)
@@ -175,18 +142,20 @@ static int rm69380_prepare(struct drm_panel *panel)
 	if (pinfo->prepared)
 		return 0;
 
-	ret = regulator_enable(pinfo->vddio);
-	if (ret) {
-		dev_err(panel->dev, "failed to enable vddio regulator: %d\n", ret);
-		return ret;
+
+	ret = regulator_bulk_enable(ARRAY_SIZE(pinfo->supplies), pinfo->supplies);
+	if (ret < 0) {
+	    dev_err(panel->dev, "failed to enable regulators: %d\n", ret);
+	    return ret;
 	}
 
 	rm69380_reset(pinfo);
 
 	ret = pinfo->desc->init_sequence(pinfo);
 	if (ret < 0) {
-		regulator_disable(pinfo->vddio);
 		dev_err(panel->dev, "failed to initialize panel: %d\n", ret);
+		gpiod_set_value_cansleep(pinfo->reset_gpio, 1);
+		regulator_bulk_disable(ARRAY_SIZE(pinfo->supplies), pinfo->supplies);
 		return ret;
 	}
 
@@ -200,12 +169,14 @@ static int rm69380_disable(struct drm_panel *panel)
 	struct panel_info *pinfo = to_panel_info(panel);
 	int i, ret;
 
+	pinfo->dsi[0]->mode_flags &= ~MIPI_DSI_MODE_LPM;
+	pinfo->dsi[1]->mode_flags &= ~MIPI_DSI_MODE_LPM;
+
 	for (i = 0; i < DSI_NUM_MIN + pinfo->desc->is_dual_dsi; i++) {
 		ret = mipi_dsi_dcs_set_display_off(pinfo->dsi[i]);
 		if (ret < 0)
 			dev_err(&pinfo->dsi[i]->dev, "failed to set display off: %d\n", ret);
 	}
-
 	msleep(35);
 
 	for (i = 0; i < DSI_NUM_MIN + pinfo->desc->is_dual_dsi; i++) {
@@ -213,7 +184,6 @@ static int rm69380_disable(struct drm_panel *panel)
 		if (ret < 0)
 			dev_err(&pinfo->dsi[i]->dev, "failed to enter sleep mode: %d\n", ret);
 	}
-
 	msleep(20);
 
 	return 0;
@@ -221,16 +191,21 @@ static int rm69380_disable(struct drm_panel *panel)
 
 static int rm69380_unprepare(struct drm_panel *panel)
 {
+	//int ret;
+
 	struct panel_info *pinfo = to_panel_info(panel);
 
 	if (!pinfo->prepared)
 		return 0;
 
+	//ret = rm69380_disable(pinfo);
+	//if (ret < 0)
+	//	dev_err(dev, "failed to un-initialize panel: %d\n", ret);
+
 	gpiod_set_value_cansleep(pinfo->reset_gpio, 1);
-	regulator_disable(pinfo->vddio);
+	regulator_bulk_disable(ARRAY_SIZE(pinfo->supplies), pinfo->supplies);
 
 	pinfo->prepared = false;
-
 	return 0;
 }
 
@@ -247,7 +222,7 @@ static void rm69380_remove(struct mipi_dsi_device *dsi)
 		ret = mipi_dsi_detach(pinfo->dsi[1]);
 		if (ret < 0)
 			dev_err(&pinfo->dsi[1]->dev, "Failed to detach from DSI1 host: %d\n", ret);
-		mipi_dsi_device_unregister(pinfo->dsi[1]);
+		//mipi_dsi_device_unregister(pinfo->dsi[1]);
 	}
 
 	drm_panel_remove(&pinfo->panel);
@@ -293,7 +268,7 @@ static const struct drm_panel_funcs rm69380_panel_funcs = {
 	.get_modes = rm69380_get_modes,
 };
 
-/*static int rm69380_bl_update_status(struct backlight_device *bl)
+static int rm69380_bl_update_status(struct backlight_device *bl)
 {
 	struct mipi_dsi_device *dsi = bl_get_data(bl);
 	u16 brightness = backlight_get_brightness(bl);
@@ -339,12 +314,11 @@ static struct backlight_device *rm69380_create_backlight(struct mipi_dsi_device 
 		.type = BACKLIGHT_RAW,
 		.brightness = 512,
 		.max_brightness = 2047,
-		//TODO .scale = BACKLIGHT_SCALE_NON_LINEAR,
 	};
 
 	return devm_backlight_device_register(dev, dev_name(dev), dev, dsi,
 					      &rm69380_bl_ops, &props);
-}*/
+}
 
 static int rm69380_probe(struct mipi_dsi_device *dsi)
 {
@@ -359,13 +333,17 @@ static int rm69380_probe(struct mipi_dsi_device *dsi)
 	if (!pinfo)
 		return -ENOMEM;
 
-	pinfo->vddio = devm_regulator_get(dev, "vddio");
-	if (IS_ERR(pinfo->vddio))
-		return dev_err_probe(dev, PTR_ERR(pinfo->vddio), "Failed to get vddio regulator\n");
+	pinfo->supplies[0].supply = "vddio";
+	pinfo->supplies[1].supply = "avdd";
+
+	ret = devm_regulator_bulk_get(dev, ARRAY_SIZE(pinfo->supplies),
+    	              pinfo->supplies);
+	if (ret < 0)
+    	return dev_err_probe(dev, ret, "failed to get regulators\n");
 
 	pinfo->reset_gpio = devm_gpiod_get(dev, "reset", GPIOD_OUT_HIGH);
 	if (IS_ERR(pinfo->reset_gpio))
-		return dev_err_probe(dev, PTR_ERR(pinfo->reset_gpio), "Failed to get reset gpio\n");
+		return dev_err_probe(dev, PTR_ERR(pinfo->reset_gpio), "failed to get reset gpio\n");
 
 	pinfo->desc = of_device_get_match_data(dev);
 	if (!pinfo->desc)
@@ -399,7 +377,7 @@ static int rm69380_probe(struct mipi_dsi_device *dsi)
 	mipi_dsi_set_drvdata(dsi, pinfo);
 	drm_panel_init(&pinfo->panel, dev, &rm69380_panel_funcs, DRM_MODE_CONNECTOR_DSI);
 
-	/*if (pinfo->desc->has_dcs_backlight) {
+	if (pinfo->desc->has_dcs_backlight) {
 		pinfo->panel.backlight = rm69380_create_backlight(dsi);
 		if (IS_ERR(pinfo->panel.backlight))
 			return dev_err_probe(dev, PTR_ERR(pinfo->panel.backlight),
@@ -408,7 +386,7 @@ static int rm69380_probe(struct mipi_dsi_device *dsi)
 		ret = drm_panel_of_backlight(&pinfo->panel);
 		if (ret)
 			return dev_err_probe(dev, ret, "Failed to get backlight\n");
-	}*/
+	}
 
 	drm_panel_add(&pinfo->panel);
 
